3ad1fb7e6aa53ad755dd8e3e8baf81ff552d45b5
Wallace Lucas <wallace.lucas@live.com>
2024-09-09 14:24:47 -0300

    Update PessoaController.java

--- src/main/java/com/meuprojeto/crudpessoas/Controller/PessoaController.java
+++ src/main/java/com/meuprojeto/crudpessoas/Controller/PessoaController.java
@@ -15,11 +15,21 @@ public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
 
+    // Comentários do código anterior:
+    // Chamar o pessoaRepository na classe Service ✔
+    // Antes de chamar o metodo que persiste, verificar se o campo email contem pelo menos um "." e apenas um "@" ✔
+    // O controller não acessa o repository, essa responsabilidade é do Service ✔
+    // Endpoint GET utilizando @PathVariable buscando por nome ✔
+    // Endpoint GET utilizando @RequestParam buscando por email ✔
+    // Estudar sobre injeção de dependencias via construtor privado. Como seria feita a injeção de mais de uma dependencia?
+
+    // Cria pessoa antigo
     /* @PostMapping
     public Pessoa criarPessoa(@RequestBody Pessoa pessoa){
         return pessoaService.criarPessoa(pessoa);
     } */
 
+    // Cria uma pessoa, verificando se o email possui pelo menos um "@" e um ".", caso contrario retorna "Email inválido"
     @PostMapping
     public ResponseEntity<?> criarPessoa(@RequestBody Pessoa pessoa) {
         Pessoa novaPessoa = pessoaService.criarPessoa(pessoa);
@@ -29,28 +39,32 @@ public class PessoaController {
         return ResponseEntity.ok(novaPessoa);
     }
 
+    // Lista todas as pessoas criadas
     @GetMapping
     public List<Pessoa> listarPessoas(){
         return pessoaService.listarPessoas();
     }
 
-    @GetMapping("/{nome}")
+    // Busca pessoas através do nome
+    @GetMapping("/buscar/{nome}")
     public Pessoa buscarPorNome(@PathVariable String nome) {
         return pessoaService.buscarPorNome(nome);
     }
 
+    // Busca pessoas através do email
     @GetMapping("/buscar")
     public Pessoa buscarPorEmail(@RequestParam String email) {
         return  pessoaService.buscarPorEmail(email);
     }
 
+    // Deleta pessoas através do ID
     @DeleteMapping("{id}")
     public ResponseEntity<Void> deletarPessoa(@PathVariable Long id){
         pessoaService.deletarPessoa(id);
         return ResponseEntity.noContent().build();
     }
 
-    // NOVO PUT
+    // Atualiza pessoas através do nome
     @PutMapping("/atualizar/{nome}")
     public ResponseEntity<Pessoa> atualizarPessoaPorNome(@PathVariable String nome, @RequestBody Pessoa pessoaAtualizada) {
         Pessoa pessoaExistente = pessoaService.buscarPorNome(nome);
