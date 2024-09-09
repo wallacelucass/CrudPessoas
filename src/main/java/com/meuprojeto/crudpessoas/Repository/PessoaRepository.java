3ffcb2f165fd2ad97b1a2815af35754a9384423c
Wallace Lucas <wallace.lucas@live.com>
2024-09-09 14:27:01 -0300

    Update PessoaRepository.java

--- src/main/java/com/meuprojeto/crudpessoas/Repository/PessoaRepository.java
+++ src/main/java/com/meuprojeto/crudpessoas/Repository/PessoaRepository.java
@@ -4,7 +4,7 @@ import com.meuprojeto.crudpessoas.Model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-    Pessoa findByName(String nome);
+    Pessoa findByNome(String nome);
 
     Pessoa findByEmail(String email);
 }
